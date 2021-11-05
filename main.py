0fc1ec0d31c3d4b06a594350f92f08f58d1561c7
Cheng Huang <huangchengadam@gmail.com>
2021-11-05 10:24:01 +0100

    change to 24

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8000, debug=False)
+    app.run(host='127.0.0.1', port=8001, debug=False)
